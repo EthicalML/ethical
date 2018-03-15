3ed2e0d94a01b55a7562b4922a0de7772dccaf2a
Alejandro Saucedo <axsauze@gmail.com>
2018-03-15 09:36:31 +0000

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -313,8 +313,8 @@
 			var mouseX = 0, mouseY = 0;
 			var windowHalfX = window.innerWidth / 2;
 			var windowHalfY = window.innerHeight / 2;
-			//init();
-			//animate();
+			init();
+			animate();
 			function init() {
 				container = document.createElement( 'div' );
                 container.style.position = "absolute";
