e08b374e33ab7cb456cc176bd346b7e69e951054
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2024-10-05 14:01:34 +0200

    Made exec inline
    Signed-off-by: Alejandro Saucedo <alejandro.saucedo@zalando.de>

--- replace_partials.sh
+++ replace_partials.sh
@@ -1,11 +1,11 @@
 
-# Add title description
-find . -path "./_*" -prune -o -name "*.html"  -exec perl -i -0777 -pe 's/<!DOCTYPE(.*|\n)*<html>/---\ntitle: The Institute for Ethical AI & Machine Learning\ndescription: The Institute for Ethical AI & Machine Learning is a Europe-based research centre that brings togethers technologists, academics and policy-makers to develop industry frameworks that support the responsible development, design and operation of machine learning systems.\n---\n<html>/g' {} +
+# 1) Add title description
+# 2) Replace header
+# 3) Remove scripts from header
 
-# Replace header
-find . -path "./_*" -prune -o -name "*.html"  -exec perl -i -0777 -pe 's/<title>(.*|\n)*itemprop="image.*>/{% include header.html %}/g' {} +
-
-# Remove scripts from header
-find . -path "./_*" -prune -o -name "*.html"  -exec perl -i -0777 -pe 's/<script(.*|\n)*main.js"><\/script>//g' {} +
+find . -path "./_*" -prune -o -name "*.html"  \
+    -exec perl -i -0777 -pe 's/<!DOCTYPE(.*|\n)*<html>/---\ntitle: The Institute for Ethical AI & Machine Learning\ndescription: The Institute for Ethical AI & Machine Learning is a Europe-based research centre that brings togethers technologists, academics and policy-makers to develop industry frameworks that support the responsible development, design and operation of machine learning systems.\n---\n<html>/g' {} + ; \
+    -exec perl -i -0777 -pe 's/<title>(.*|\n)*itemprop="image.*>/{% include header.html %}/g' {} +; \
+    -exec perl -i -0777 -pe 's/<script(.*|\n)*main.js"><\/script>//g' {} +;
 
 
