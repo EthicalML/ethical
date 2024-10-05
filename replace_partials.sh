f8503abd9c548170520a50f6fc31381802252c09
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2024-10-05 14:13:19 +0200

    Finalised replace for all pages
    Signed-off-by: Alejandro Saucedo <alejandro.saucedo@zalando.de>

--- replace_partials.sh
+++ replace_partials.sh
@@ -1,11 +1,22 @@
 
-# 1) Add title description
-# 2) Replace header
-# 3) Remove scripts from header
+multiple_cmd() { 
+    # Add title description
+    perl -i -0777 -pe 's/<!DOCTYPE(.*|\n)*<html>/---\ntitle: The Institute for Ethical AI & Machine Learning\ndescription: The Institute for Ethical AI & Machine Learning is a Europe-based research centre that brings togethers technologists, academics and policy-makers to develop industry frameworks that support the responsible development, design and operation of machine learning systems.\n---\n<html>/g' $1
 
-find . -path "./_*" -prune -o -name "*.html"  \
-    -exec perl -i -0777 -pe 's/<!DOCTYPE(.*|\n)*<html>/---\ntitle: The Institute for Ethical AI & Machine Learning\ndescription: The Institute for Ethical AI & Machine Learning is a Europe-based research centre that brings togethers technologists, academics and policy-makers to develop industry frameworks that support the responsible development, design and operation of machine learning systems.\n---\n<html>/g' {} + ; \
-    -exec perl -i -0777 -pe 's/<title>(.*|\n)*itemprop="image.*>/{% include header.html %}/g' {} +; \
-    -exec perl -i -0777 -pe 's/<script(.*|\n)*main.js"><\/script>//g' {} +;
+    # Replace header for partial
+    perl -i -0777 -pe 's/<title>(.*|\n)*itemprop="image.*>/{% include header.html %}/g' $1
+
+    # Replace navbar for partial
+    perl -0777 -pe 's/<!-- Header(.*|\n)*<\/nav>\s*<\/header>/{% include navbar.html %}/g' $1
+
+    # Remove scripts from header
+    perl -i -0777 -pe 's/<script(.*|\n)*main.js"><\/script>//g' $1
+
+    # Replace footer with partial
+    perl -0777 -pe 's/<!-- Footer(.*|\n)*<\/script>/{% include footer.html %}/g' $1
+}
+export -f multiple_cmd
+
+find . -path "./_*" -prune -o -name "*.html" -exec bash -c 'multiple_cmd "$0"' {} \;
 
 
